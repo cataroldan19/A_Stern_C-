69afc29c936df3b493131c6cbae382b2aea61df2
martin.moraga <martin.moraga.91@gmail.com>
2018-12-15 13:07:00 +0100

    added new file grid.cpp

---
+++ grid.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#include "Graph.h"
+#include "Knoten.cpp"
+
+void draw_grid(const Graph& graph){
+	
+	// show start point
+	std::vector<std::vector<std::string>> vec2D(graph.length, std::vector<std::string>(graph.width, "#"));
+	vec2D[graph.startPoint.getY][graph.startPoint.getX] = "S ";
+	
+	std::queue<Knoten*> notSearched;
+	std::vector<Knoten*> searched;
+	
+	notSearched.push(graph.startPoint);
+	while (!notSearched.empty) {
+		Knoten current = notSearched.pop();
+		vec2D[current.getY][current.getX] = " #";
+		for(int i=0; i!=graph.knoten.neighbors.size(); i++) {
+			// Knoten *temp =  graph.knoten.neighbors[i];
+			Knoten *temp =  graph.knoten.neighbors[i];
+			if (std::find(searched.begin(), searched.end(), temp) == searched.end()) {		//Verificar!!!
+				notSearched.push(test);
+			}
+		}
+	}
+	
+	for(int i=0; i<vec2D.size(); i++) {
+		for(int j=0; j<vec2D[i].size(); j++) {
+			std::cout << vec2D[i][j];
+		}
+		std::cout << "\n";
+	}
+}
