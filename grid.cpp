74fe77a11d253a157b9b911b5268a679ab8639f2
martin.moraga <martin.moraga.91@gmail.com>
2018-12-15 13:12:46 +0100

    fixed errors in grid.cpp

--- grid.cpp
+++ grid.cpp
@@ -3,26 +3,25 @@
 #include <algorithm>
 
 #include "Graph.h"
-#include "Knoten.cpp"
+#include "Knoten.h"
 
 void draw_grid(const Graph& graph){
 	
-	// show start point
 	std::vector<std::vector<std::string>> vec2D(graph.length, std::vector<std::string>(graph.width, "#"));
-	vec2D[graph.startPoint.getY][graph.startPoint.getX] = "S ";
+	vec2D[graph.startPoint.getY()][graph.startPoint.getX()] = "S ";
 	
 	std::queue<Knoten*> notSearched;
 	std::vector<Knoten*> searched;
 	
 	notSearched.push(graph.startPoint);
-	while (!notSearched.empty) {
+	while (!notSearched.empty()) {
 		Knoten current = notSearched.pop();
-		vec2D[current.getY][current.getX] = " #";
+		vec2D[current.getY()][current.getX()] = " #";
 		for(int i=0; i!=graph.knoten.neighbors.size(); i++) {
 			// Knoten *temp =  graph.knoten.neighbors[i];
 			Knoten *temp =  graph.knoten.neighbors[i];
 			if (std::find(searched.begin(), searched.end(), temp) == searched.end()) {		//Verificar!!!
-				notSearched.push(test);
+				notSearched.push(temp);
 			}
 		}
 	}
